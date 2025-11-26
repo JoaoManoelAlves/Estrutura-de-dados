475b426c8fdac297ea8294fd6ba7aba8fe9d4812
Bruno V <vitalb@acad.ifma.edu.br>
2025-11-26 15:29:19 -0300

    Refactor DoublyLinkedList implementation

--- DoublyLinkedList/doublylinkedlist.js
+++ DoublyLinkedList/doublylinkedlist.js
@@ -73,13 +73,6 @@ class DoublyLinkedList extends LinkedList {
 
 }
 
-const listadupla = new DoublyLinkedList();
-listadupla.inserir(1,0);
-listadupla.inserir(2,1);
-listadupla.inserir(3,2);
-listadupla.inserir(4,3);
-console.log(listadupla);
-console.log(listadupla.elementos());
 
 
 
