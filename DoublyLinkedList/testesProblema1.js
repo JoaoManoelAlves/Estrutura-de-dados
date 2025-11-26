ef3d80eb4f0184ea9f25c031375d9717f86dd48b
Ester Martins <sterre.msb@gmail.com>
2025-11-26 16:37:45 -0300

    Delete DoublyLinkedList/testesProblema1.js

--- DoublyLinkedList/testesProblema1.js
+++
@@ -1,7 +0,0 @@
-const listadupla = new DoublyLinkedList();
-listadupla.inserir(1,0);
-listadupla.inserir(2,1);
-listadupla.inserir(3,2);
-listadupla.inserir(4,3);
-console.log(listadupla);
-console.log(listadupla.elementos());
