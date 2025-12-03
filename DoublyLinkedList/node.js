39c89ad66d0d48bc5808fc82d5db4c26b687b31a
JoaoManoelAlves <162204418+JoaoManoelAlves@users.noreply.github.com>
2025-12-03 14:21:57 -0300

    Delete DoublyLinkedList/node.js
    Não será contabilizado. O NodeList está no mesmo arquivo que a DoubleLinkedList

--- DoublyLinkedList/node.js
+++
@@ -1,7 +0,0 @@
-//comando module.exports serve pra permitir que a classe seja chamada em outros arquivos
-module.exports = class Node {
-  constructor(element, next) {
-    this.element = element;
-    this.next = next;
-  }
-};
