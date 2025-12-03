f0fb647ef8aa63e2e4a8fe0a311787479c7ae3fd
JoaoManoelAlves <162204418+JoaoManoelAlves@users.noreply.github.com>
2025-12-03 14:22:58 -0300

    Refactor and implement DoubleLinkedList class
    Refatoração do código da DoubleLinkedList

--- DoublyLinkedList/doublylinkedlist.js
+++ DoublyLinkedList/doublylinkedlist.js
@@ -1,101 +1,104 @@
-//A classe doublylinkedlist é um tipo especial de LinkedList que nós já conhecemos, portanto iremos importar as propriedades e métodos já desenvolvidos em sala de aula e adicionar a nova função que irá ligar o nó ao seu elemento anterior (prev).
-
-const Node = require("./node.js");
-const LinkedList = require("./linkedlist.js");
-
-function defaultEquals(a, b) {
-    return a === b;
-}
-
-class DoublyNode extends Node {
-    constructor(element, next, previous) {
-        super(element, next);
-        this.previous = previous;
+class NodeList{
+    constructor(data){
+        this.data = data
+        this.next = null
+        this.prev = null
     }
 }
-class DoublyLinkedList extends LinkedList{
-    constructor(equalsFn = defaultEquals){
-        super(equalsFn);
-        this.tail = undefined; //Além disso, precisamos manter também uma referência ao último elemento da lista (tail).
+class DoubleLinkedList{
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.count = 0
     }
-    //Inserindo um novo elemento em qualquer posição: 
-    // Na classe doublylinkedlist, iremos alterar o método inserir que conhecemos da LinkedList comum, pois além de controlar a propriedade next, temos a adição do ponteiro prev que indica o elemento anterior.
-    
-    inserir(element, index) {
-        if (index >= 0 && index <= this.count) {
-            const node = new DoublyNode(element);
-            let current = this.head;
-            if (index === 0) {
-                if (this.head == null) {
-                    this.head = node;
-                    this.tail = node;
-                } else {
-                    node.next = this.head;
-                    current.previous = node;
-                    this.head = node;
-                }
 
-            } else if (index === this.count) {
-                current = this.tail;
-                current.next = node;
-                node.previous = current;
-                this.tail = node;
-            } else {
-                const previous = this.getElementAt(index - 1);
-                current = previous.next;
-                node.next = current;
-                previous.next = node;
-                current.previous = node;
-                node.previous = previous;
-            }
-            this.count++;
-            return true;
+    insertEnd(element){
+        //Cria o novo nó
+        const newNo = new NodeList(element)
+        //Se não haver cabeça
+        if(this.head == null){
+            //Cabeça é criada
+            this.head = newNo
+            //A cauda aponta para a cabeça
+            this.tail = newNo
+        }else{//Se houver
+            //o next da cauda aponta para o novo nó
+            this.tail.next = newNo
+            //O previous do novo nó aponta para a antiga cauda
+            newNo.prev = this.tail
+            //A nova cauda recebe o novo nó
+            this.tail = newNo
         }
-        return false;
+        this.count++
     }
-    
-    removeAt(index) {
-        if (index >= 0 && index < this.count) {
-            let current = this.head;
-            if (index === 0) {
-                this.head = current.next; 
-            }else if(this.count === 1) { // Se houver apenas um item atualiza-se o tail         
-                this.tail = undefined;
-            }else{
-                this.head.previous = undefined;
-            }
-        }else{
-            current = this.tail;
-            this.tail = current.previous; 
-            this.tail.next = undefined; 
+    insertFront(element){
+        if(this.isEmpty()) return this.insertEnd(element)
+        const newNo = new NodeList(element)
+        newNo.next = this.head
+        this.head.prev = newNo
+        this.head = newNo
+        this.count++
+    }
+    insertAnyWhere(index, element){
+        if (index < 0 || index > this.count) return undefined
+        if(index === 0) return this.insertFront(element)
+        if(index === this.size()) return this.insertEnd(element)
+        
+        const newNo = new NodeList(element)
+        const currentNode = this.getNodeAt(index)
+
+        const previousNode = currentNode.prev
+
+        newNo.prev = previousNode
+        newNo.next = currentNode
+
+        currentNode.prev = newNo
+        previousNode.next = newNo
+        
+        this.count++
+    }
+    deleteFront() {
+        //Deletará o primeiro nó
+    }
+    deleteBack(){
+        //Deletará o ultimo nó
+    }
+    deleteAnyWhere(){
+        //Deletará de qualquer nó
+    }
+    getNodeAt(index){
+        if (index < 0 || index >= this.count) return undefined
+        let atual = this.head
+        let i=0
+        while(i<index){
+            atual = atual.next
+            i++
         }
+        return atual
+    }
+    cabeca(){
+        return this.head
+    }
+    cauda(){
+        return this.tail
+    }
+    size(){
+        return this.count
+    }
+    isEmpty(){
+        return this.count === 0
+    }
+    clear(){
+        this.head = null
+        this.tail = null
+        this.count = 0
+    }
 }
- getNext(node) {
-    return node ? node.next : null;
-  }
 
-  getPrev(node) {
-    return node ? node.prev : null;
-
-
-                //Obrigatorios 
-  isEmpty() {
-    return this.count === 0;
-  }
-
-  size() {
-    return this.count;
-  }
-
-  clear() {
-    this.head = null;
-    this.tail = null;
-    this.count = 0;
-  }
-
-
-
-
-    
-}
+const doubleLinkedList = new DoubleLinkedList
+doubleLinkedList.insertEnd(2)
+doubleLinkedList.insertEnd(3)
+doubleLinkedList.insertEnd(4)
+doubleLinkedList.insertAnyWhere(3, 5)
+console.log(doubleLinkedList)
 
