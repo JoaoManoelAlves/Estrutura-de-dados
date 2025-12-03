ae59e1b5d00a6892293e50c0b55b2706d8d09788
andre-luis-araujo <andre.araujo1@acad.ifma.edu.br>
2025-12-03 15:14:52 -0300

    Refactor DoublyLinkedList for code consistency

--- DoublyLinkedList/ListaDuplamenteEncadeada/ListaDuplamenteEncadeada.js
+++ DoublyLinkedList/ListaDuplamenteEncadeada/ListaDuplamenteEncadeada.js
@@ -1,104 +1,117 @@
-class NodeList{
-    constructor(data){
-        this.data = data
-        this.next = null
-        this.prev = null
+class NodeList {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
     }
 }
-class DoubleLinkedList{
-    constructor(){
-        this.head = null
-        this.tail = null
-        this.count = 0
+class DoubleLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.count = 0;
     }
 
-    insertEnd(element){
+    insertEnd(element) {
         //Cria o novo nó
-        const newNo = new NodeList(element)
+        const newNo = new NodeList(element);
         //Se não haver cabeça
-        if(this.head == null){
+        if (this.head == null) {
             //Cabeça é criada
-            this.head = newNo
+            this.head = newNo;
             //A cauda aponta para a cabeça
-            this.tail = newNo
-        }else{//Se houver
+            this.tail = newNo;
+        } else {
+            //Se houver
             //o next da cauda aponta para o novo nó
-            this.tail.next = newNo
+            this.tail.next = newNo;
             //O previous do novo nó aponta para a antiga cauda
-            newNo.prev = this.tail
+            newNo.prev = this.tail;
             //A nova cauda recebe o novo nó
-            this.tail = newNo
+            this.tail = newNo;
         }
-        this.count++
+        this.count++;
     }
-    insertFront(element){
-        if(this.isEmpty()) return this.insertEnd(element)
-        const newNo = new NodeList(element)
-        newNo.next = this.head
-        this.head.prev = newNo
-        this.head = newNo
-        this.count++
-    }
-    insertAnyWhere(index, element){
-        if (index < 0 || index > this.count) return undefined
-        if(index === 0) return this.insertFront(element)
-        if(index === this.size()) return this.insertEnd(element)
-        
-        const newNo = new NodeList(element)
-        const currentNode = this.getNodeAt(index)
+    insertFront(element) {
+    if (this.isEmpty()) return this.insertEnd(element);
+    const newNo = new NodeList(element);
+    newNo.next = this.head;
+    this.head.prev = newNo;
+    this.head = newNo;
+    this.count++;
+}
+    insertAnyWhere(index, element) {
+        if (index < 0 || index > this.count) return undefined;
+        if (index === 0) return this.insertFront(element);
+        if (index === this.size()) return this.insertEnd(element);
 
-        const previousNode = currentNode.prev
+        const newNo = new NodeList(element);
+        const currentNode = this.getNodeAt(index);
 
-        newNo.prev = previousNode
-        newNo.next = currentNode
+        const previousNode = currentNode.prev;
 
-        currentNode.prev = newNo
-        previousNode.next = newNo
-        
-        this.count++
+        newNo.prev = previousNode;
+        newNo.next = currentNode;
+
+        currentNode.prev = newNo;
+        previousNode.next = newNo;
+
+        this.count++;
+
     }
     deleteFront() {
         //Deletará o primeiro nó
     }
-    deleteBack(){
+    deleteBack() {
         //Deletará o ultimo nó
     }
-    deleteAnyWhere(){
+    deleteAnyWhere() {
         //Deletará de qualquer nó
     }
-    getNodeAt(index){
-        if (index < 0 || index >= this.count) return undefined
-        let atual = this.head
-        let i=0
-        while(i<index){
-            atual = atual.next
-            i++
+    getNodeAt(index) {
+        if (index < 0 || index >= this.count) return undefined;
+        let atual = this.head;
+        let i = 0;
+        while (i < index) {
+            atual = atual.next;
+            i++;
         }
-        return atual
+        return atual;
+}
+    cabeca() {
+        return this.head;
     }
-    cabeca(){
-        return this.head
-    }
-    cauda(){
-        return this.tail
-    }
-    size(){
-        return this.count
-    }
-    isEmpty(){
-        return this.count === 0
-    }
-    clear(){
-        this.head = null
-        this.tail = null
-        this.count = 0
+    cauda() {
+        return this.tail;
+}
+    size() {
+        return this.count;
+}
+    isEmpty() {
+        return this.count === 0;
+}
+    clear() {
+        this.head = null;
+        this.tail = null;
+        this.count = 0;
+}
+    elementos() {
+        let atual = this.head;
+        let elementos = [];
+        while (atual !== null) {
+            elementos.push(atual)
+            //elementos.push(atual.data)
+            //elementos.push("<=>");
+            atual = atual.next;
+        }
+        return console.log(elementos);
     }
 }
 
-const doubleLinkedList = new DoubleLinkedList
-doubleLinkedList.insertEnd(2)
-doubleLinkedList.insertEnd(3)
-doubleLinkedList.insertEnd(4)
-doubleLinkedList.insertAnyWhere(3, 5)
-console.log(doubleLinkedList)
-
+const doubleLinkedList = new DoubleLinkedList();
+doubleLinkedList.insertEnd(2);
+doubleLinkedList.insertEnd(3);
+doubleLinkedList.insertEnd(4);
+doubleLinkedList.insertAnyWhere(3, 5);
+doubleLinkedList.insertEnd(99);
+doubleLinkedList.elementos();
