7de2e06e17ec0ffed54cbe4b33025f1f398207bd
JoaoManoelAlves <162204418+JoaoManoelAlves@users.noreply.github.com>
2025-12-09 17:19:10 -0300

    Refactor tests for DoublyLinkedList and PlayList
    Updated tests for doubly linked list and playlist functionality.

--- DoublyLinkedList/TesteProblema/testeProblema1.js
+++ DoublyLinkedList/TesteProblema/testeProblema1.js
@@ -1,7 +1,26 @@
-const listadupla = new DoublyLinkedList();
-listadupla.inserir(1,0);
-listadupla.inserir(2,1);
-listadupla.inserir(3,2);
-listadupla.inserir(4,3);
-console.log(listadupla);
-console.log(listadupla.elementos());
+const doubleLinkedlist  = require("./DoubleLinkedList.js")
+const playList = require("./solucaoProblema1.js")
+// Testes da lista Duplamente Linkada Normal
+const doubleLinkedList = new doubleLinkedlist();
+doubleLinkedList.insertEnd(1);
+doubleLinkedList.insertEnd(2);
+doubleLinkedList.insertEnd(3);
+doubleLinkedList.insertAnyWhere(3,4);
+doubleLinkedList.insertEnd(5);
+doubleLinkedList.elementos();
+doubleLinkedList.deleteAnyWhere(4);
+doubleLinkedList.elementos();*/
+
+// Testes da PlayList de Músicas
+const PlayList = new playList()
+PlayList.addSong("Música 1")
+PlayList.addSong("Música 2")
+PlayList.addSong("Música 3")
+PlayList.addSong("Música 4")
+console.log(PlayList)
+PlayList.showPlaylist()//Saída esperada: Musica 1 <-> Musica 2 <-> Musica 3 <-> Musica 4 e tocando agora música 1
+PlayList.removeCurrent()
+PlayList.showPlaylist()//Musica 2 <-> Musica 3 <-> Musica 4 e tocando agora música 2
+PlayList.reset()//Lista vazia e sem música selecionada
+PlayList.showPlaylist()
+console.log(PlayList)
